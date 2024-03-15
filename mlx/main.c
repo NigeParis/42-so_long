f266dc5955025aad21895a435042270b06543887
Nigel Robinson <nrobinso@paul-f4Ar8s7.clusters.42paris.fr>
2024-03-15 13:26:01 +0100

    add mlx

---
+++ mlx/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 16:25:39 by nrobinso          #+#    #+#             */
+/*   Updated: 2024/02/02 17:07:04 by nrobinso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "mlx.h"
+#include <stdlib.h>
+ 
+int main(void)
+{
+	void *mlx_ptr;
+	void *win_ptr;
+ 
+	mlx_ptr = mlx_init();
+	if (!mlx_ptr)
+		return (1);
+	win_ptr = mlx_new_window(mlx_ptr, 600, 400, "hi :)");
+	if (!win_ptr)
+		return (free(mlx_ptr), 1);
+	mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
+	return (0);
+}
